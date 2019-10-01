f77d4334b501446c7edba597f5a73b84cea4c8f4
tranhoangtruc1812 <54313021+tranhoangtruc1812@users.noreply.github.com>
2019-10-01 23:19:38 +0700

    bai tap lab 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,35 @@ import {RouterModule} from '@angular/router'
 import { AppComponent } from './app.component';
 import { ListSVComponent } from './list-sv/list-sv.component';
 import { StudentsComponent } from './students/students.component';
+import {FormsModule} from '@angular/forms';
+import { HinhchunhatComponent } from './hinhchunhat/hinhchunhat.component';
+import { SinhvienComponent } from './sinhvien/sinhvien.component';
+import { EmployerComponent } from './employer/employer.component';
+import { BanhangComponent } from './banhang/banhang.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListSVComponent,
-    StudentsComponent
+    StudentsComponent,
+    HinhchunhatComponent,
+    SinhvienComponent,
+    EmployerComponent,
+    BanhangComponent
+
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot([
       {path:'', component:ListSVComponent},
-      {path:'home', component:ListSVComponent},
-      {path:'home/sinhvien/:id', component:StudentsComponent},
+      {path:'lab2', component:ListSVComponent},
+      {path:'lab3', component: HinhchunhatComponent},
+      {path:'lab3/hinhchunhat', component:HinhchunhatComponent},
+      {path:'lab3/sinhvien', component:SinhvienComponent},
+      {path:'lab3/tienthuong', component:EmployerComponent},
+      {path:'lab3/banhang', component:BanhangComponent},
+      {path:'lab2/sinhvien/:id', component:StudentsComponent},
       {path:'**',redirectTo: 'home', pathMatch: 'full' },
 
       
---
+++ src/app/employer/employer.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employer',
+  templateUrl: './employer.component.html',
+  styleUrls: ['./employer.component.css']
+})
+export class EmployerComponent implements OnInit {
+  work ={
+    name: null,
+    gender: null,
+    luong: null,
+    rate:null
+  } 
+  
+    constructor() { }
+  
+    ngOnInit() {
+    }
+    list = [
+      {
+        age: 'dưới 25 tuổi',
+        rate: 0.07
+      },
+      {
+        age: 'từ 25 đến 40 tuổi',
+        rate: 0.1
+      },
+      {
+        age: 'trên 40 tuổi',
+        rate: 0.15  
+      }
+    ]
+    tienthuong(){
+      let luong
+      if(this.work.luong >= 2000000){
+       luong =  this.work.rate * this.work.luong }
+      if(this.work.gender === 'Nữ')
+      luong += 200000;
+      return luong
+    }
+  
+}
